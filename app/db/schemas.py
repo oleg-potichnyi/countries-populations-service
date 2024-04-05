efe2772fce85aa74d03a3f32faee2ca62a9d6bf2
oleg-potichnyi <potichnyi.oleg@gmail.com>
2024-04-05 18:23:51 +0300

    Add SQLAlchemy configuration in schemas.py

---
+++ app/db/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class CountryBase(BaseModel):
+    name: str
+    population: int
+
+
+class CountryCreate(CountryBase):
+    pass
+
+
+class CountryList(CountryBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+        
