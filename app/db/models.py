c7297f2180a59f97002f3cb7e379c98f2af64971
oleg-potichnyi <potichnyi.oleg@gmail.com>
2024-04-05 18:21:34 +0300

    Add database model for countries

---
+++ app/db/models.py
@@ -0,0 +1,10 @@
+from app.db.engine import Base
+from sqlalchemy import Column, Integer, String, BigInteger
+
+
+class DBCountry(Base):
+    __tablename__ = "countries"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False, unique=True)
+    population = Column(BigInteger)
